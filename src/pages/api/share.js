5d1c311419a609bd8f0a35544c1671c576bcf146
Md Salik Inam <salikinam@gmail.com>
2025-08-17 01:10:26 +0530

    New1

--- src/pages/api/share.js
+++ src/pages/api/share.js
@@ -1,6 +1,6 @@
 import { Resend } from "resend";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+const resend = new Resend("re_6XBdiYqV_FREJXz7KUFVYjqjfra1NxXd5");
 
 export default async function handler(req, res) {
   if (req.method !== "POST") return res.status(405).end();
